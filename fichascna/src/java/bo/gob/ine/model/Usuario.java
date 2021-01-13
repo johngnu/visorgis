2ee0e18b358a72436caaf3da0e8535f6e61050eb
John Castillo <john.gnu@gmail.com>
2021-01-13 13:45:19 -0400

    create project

---
+++ fichascna/src/java/bo/gob/ine/model/Usuario.java
@@ -0,0 +1,150 @@
+/*
+ * Essential SRL - 2016
+ */
+package bo.gob.ine.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Edy Huiza
+ */
+public class Usuario implements Serializable {
+
+    private Integer id;
+    private String nombres;
+    private String apellidos;
+    private String cargo;
+    private String descripcion;
+    private String usuario;
+    private String email;
+    private String telefonos;
+    private String domicilio;
+    private String tipo_documento;
+    private String nro_documento;
+    private String sexo;
+    private String rol;
+    private String clave;
+    private Boolean activo = false;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefonos() {
+        return telefonos;
+    }
+
+    public void setTelefonos(String telefonos) {
+        this.telefonos = telefonos;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getTipo_documento() {
+        return tipo_documento;
+    }
+
+    public void setTipo_documento(String tipo_documento) {
+        this.tipo_documento = tipo_documento;
+    }
+
+    public String getNro_documento() {
+        return nro_documento;
+    }
+
+    public void setNro_documento(String nro_documento) {
+        this.nro_documento = nro_documento;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public Boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
+
+}
